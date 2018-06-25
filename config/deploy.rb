3b0b1169d8eaa6345608a9a5ce8ee880939792b6
FranZisko <jfelmejor@gmail.com>
2018-06-25 18:27:50 -0400

    e

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-
+server "178.128.161.139", primary :true
 set :application, 'Proyecto'
 set :repo_url, 'git@github.com:FranZisko/Proyecto.git'
 set :user, 'root'
@@ -12,7 +12,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/rails/rails_project"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
